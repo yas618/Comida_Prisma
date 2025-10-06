39c2c8e6949ba4e95de07aea5b53f71d9ea71ccf
Yasmin Porto <yasmin.porto@aluno.senai.br>
2025-10-06 15:53:34 -0300

    comida prisma

---
+++ prisma/migrations/20251006175905_npx_prisma_migrate_dev_name_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "comidas" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "tipo" TEXT NOT NULL,
+    "preco" DOUBLE PRECISION NOT NULL,
+    "descricao" TEXT NOT NULL,
+
+    CONSTRAINT "comidas_pkey" PRIMARY KEY ("id")
+);
---
+++ server.js
@@ -0,0 +1,25 @@
+// Importar pacotes/bibliotecas
+import express from "express";
+import dotenv from "dotenv";
+
+// Criar aplicação com Express e configurar para aceitar JSON
+const app = express();
+app.use(express.json());
+
+// Carregar variáveis de ambiente e definir constante para porta do servidor
+dotenv.config();
+const serverPort = process.env.PORT || 3001;
+
+// Rota principal GET para "/"
+app.get("/", (req, res) => {
+    res.send("🚀 Servidor funcionando...");
+});
+
+
+// Aqui vão todas suas Rotas
+
+
+// Iniciar servidor escutando na porta definida
+app.listen(serverPort, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${serverPort} 🚀`);
+});
